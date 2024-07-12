57128bfd6ec40ad5df0c899704a64868a8b01388
Guillot_Natt <natthan.guillot@fr.glory-global.com>
2024-07-12 16:12:04 +0200

    Feature(Test_Client): removing useless client option

--- src/Test_Client/client.py
+++ src/Test_Client/client.py
@@ -7,15 +7,13 @@ def cli():
     pass
 
 @cli.command()
-@click.option("--path", "-p", help="Entrez le chemin d'accès au fichier", type=click.Path(), default="cycle_1")
-@click.option("--size", "-s", help="Entrez la taille des données à écrire", type=int, default=10000)
+@click.option("--size", "-s", help="Entrez la taille des données à écrire", type=int, default=1000)
 @click.option("--method", "-m", help="Entrez la méthode d'écriture à utiliser", default="None")
 @click.option("--iteration", "-i", help="Entrez le numéro de l'itération", default="first")
-def writetx(path, size, method, iteration):
+def writetx(size, method, iteration):
     # Construire la commande:
     commandBits = []
     commandBits.append("-writeTx")
-    commandBits.append(f"-path={path}")
     commandBits.append(f"-size={size}")
     commandBits.append(f"-method={method}")
     commandBits.append(f"-iteration={iteration}")
@@ -44,8 +42,8 @@ def send_command(command: str, host='192.168.0.141', port=65432):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((host, port))
         s.sendall(command.encode('utf-8'))
-        data = s.recv(4096)
-        print(f"\nReceived output:\n{data.decode('iso-8859-1')}\n")
+        # data = s.recv(4096)
+        # print(f"\nReceived output:\n{data.decode('iso-8859-1')}\n")
 
 if __name__ == "__main__":
     cli()
