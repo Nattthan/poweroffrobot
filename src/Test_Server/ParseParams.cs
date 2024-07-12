1de9ee393e9176299fe7c72eb265be738796a0db
Guillot_Natt <natthan.guillot@fr.glory-global.com>
2024-07-12 16:17:08 +0200

    Quality(Test_Server): Creating a file that manages gathering all the parameters sent by the client to lighten the main

---
+++ src/Test_Server/ParseParams.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransactionalBankingSimulation
+{
+  public class ParseParams
+  {
+    public static bool methodWriteTx = false;
+    public static bool methodAnalyze = false;
+    public static string size = "";
+    public static string writingMethod = "";
+    public static string iteration = "";
+
+    // Gather the arguments passed to the program
+    public static void parse(IEnumerable<string> args)
+    {
+      foreach (string arg in args)
+      {
+        if (arg == "-writeTx")
+        {
+          methodWriteTx = true;
+        }
+        else if (arg == "-analyze")
+        {
+          methodAnalyze = true;
+        }
+        else if (arg.StartsWith("-size="))
+        {
+          size = arg.Substring(6);
+        }
+        else if (arg.StartsWith("-method="))
+        {
+          writingMethod = arg.Substring(8); ;
+        }
+        else if (arg.StartsWith("-iteration="))
+        {
+          iteration = arg.Substring(11);
+        }
+
+      }
+    }
+  }
+}
